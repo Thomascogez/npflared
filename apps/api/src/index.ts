729e808fa8cbb32e8a99414fe5a3049d0ac2a68d
Thomas Cogez <thomas.cogez@gmail.com>
2026-01-12 09:55:59 +0100

    refactor: trigger token load middleware into global router

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
+import { loadToken } from "#middlewares/loadToken";
 import { packageRouter } from "./routers/package";
 import { tokenRouter } from "./routers/token";
 
 const app = new Hono();
+
+app.use("*", loadToken);
 
 const routes = app.route("/", tokenRouter).route("/", packageRouter);
 
