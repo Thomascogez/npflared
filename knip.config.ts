398cac4c8ff96c4720235fef9fefb097a3bcefdf
Thomas Cogez <thomas.cogez@gmail.com>
2026-05-13 16:24:22 +0200

    chore: ignore relations file

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,11 @@ import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
 	ignoreDependencies: ["cloudflare"],
-    ignore: ["./apps/doc/rspress.config.ts"]
+    ignore: [
+        "./apps/doc/rspress.config.ts",
+        
+        "./app/api/src/db/relations.ts"
+    ]
 };
 
 export default config;
